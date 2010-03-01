7ee11eb640420678d1515181bfa450c8a633d97a
Fredrik Rubensson <fredrik@eldfluga.se>
2010-03-01 19:16:39 +0100

    More testing

--- vendor/extensions/dog/spec/models/dog_spec.rb
+++ vendor/extensions/dog/spec/models/dog_spec.rb
@@ -1,28 +1,59 @@
 
-describe Dog, "#new" do
-  
-  after :all do
-    Dog.destroy_all
+module DogCreationHelper
+  def create_dog
+    @dog = Dog.new 
+    @dog.name = 'Fido Two'
+    @dog.gender = 'M'
+    @dog.description = 'Fido is a nice dog'
+    @dog.birthdate = Date.new 2010, 2, 2
+    @dog.asset_id = 1
   end
   
-  it "finds a new one" do
-    dog = Dog.new 
-    dog.name = 'Fido Two'
-    dog.gender = 'M'
-    dog.description = 'Fido is a nice dog'
-    dog.birthdate = Date.new 2010, 2, 2
+  def kill_dog
+    @dog.destroy
+  end
+end
 
-    dog.save
+describe Dog, "when creating and saving new ones" do
+  include DogCreationHelper
+  
+  before :each do
+    create_dog
+  end
+  
+  it "should be findable" do
+
+    @dog.save
     
     foundfido = Dog.find :first, :conditions => ['name = "Fido Two"']
-    foundfido.name.should == dog.name
-    foundfido.gender.should == dog.gender
-    foundfido.description.should == dog.description
-    foundfido.birthdate.should == dog.birthdate
+    foundfido.name.should == @dog.name
+    foundfido.gender.should == @dog.gender
+    foundfido.description.should == @dog.description
+    foundfido.birthdate.should == @dog.birthdate
   end
   
-  it "ensures all fields are mandatory" do
+  it "makes sure all fields are mandatory" do
     dog = Dog.new 
     lambda { dog.save }.should raise_exception
+  end
+  
+  after :each do
+    kill_dog
+  end
+end
+
+describe Dog, "when having a picture" do
+  include DogCreationHelper
+  
+  before :each do
+    create_dog
+  end
+  
+  it "should return a nice picture file name" do
+    @dog.picture_file_name.should match /\.\.\/assets\/\d+\/\w+.(jpg|gif)/
+  end
+  
+  after :each do
+    kill_dog
   end
 end
