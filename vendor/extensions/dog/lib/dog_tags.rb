733e97947514bf224b0f6fce89605f1b52387b65
Hardy <hardy@ferentschik.de>
2010-03-18 23:28:48 -0300

    translated months

--- vendor/extensions/dog/lib/dog_tags.rb
+++ vendor/extensions/dog/lib/dog_tags.rb
@@ -17,4 +17,26 @@ module DogTags
     content << '</ul></center>'
     return content
   end
+  
+   # This is a ugly hack. Really this tag should have its own extension. It has
+   # nothing to do with dogs. Translate is only a crutch or translating months names into Spanish
+   # since the i18n module of rails is not ready yet.
+  desc "Temporary tag for translating dates from <r:date> to Spanish"
+  tag "translate" do |tag|
+    content = ''
+    content << tag.expand
+    content.gsub!("January", "Enero")
+    content.gsub!("February", "Febrero")
+    content.gsub!("March", "Marzo")
+    content.gsub!("April", "April")
+    content.gsub!("May", "Mayo")
+    content.gsub!("June", "Junio")
+    content.gsub!("July", "Julio")
+    content.gsub!("August", "Agosto")
+    content.gsub!("September", "Septiembre")
+    content.gsub!("October", "Octubre")
+    content.gsub!("November", "Noviembre")
+    content.gsub!("December", "Diciembre")
+    return content
+  end
 end
